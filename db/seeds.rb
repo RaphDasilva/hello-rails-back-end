a069255969c217d8095d7b186926616a788e8865
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-06-06 10:57:16 +0100

    Created database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(text: "Hello, how are you today?")
+Message.create(text: "Good morning! I hope you're having a great day.")
+Message.create(text: "Hi there! It's nice to see you.")
+Message.create(text: "Greetings! I trust everything is going well for you.")
+Message.create(text: "Hey! Wishing you a fantastic day ahead.")
